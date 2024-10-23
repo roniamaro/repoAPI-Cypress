f14b5fb1d2641eb4904ef014c50e4246702be26c
roni_amaro <roni_amaro@hotmail.com>
2024-10-23 11:23:12 -0300

    Feat: novas configurações iniciais Cypress para Iniciantes - e2e tests em frontend apps Erick Wendel

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: '', // seta a config de url que será testada -> "cy.visit('/')"
+    testIsolation: false // não vai limpar o estado da tela após cada IT
   },
 });
